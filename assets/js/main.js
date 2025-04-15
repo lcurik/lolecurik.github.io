6a2cd428d2c2c4a0043a290af3267686ec63d689
Lole Curik <lcurik@berklee.edu>
2025-04-15 18:47:18 -0400

    updated fixed. now trying to do hamburger button

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,218 +3,208 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$all = $body.add($header);
+    var $window = $(window),
+        $body = $('body'),
+        $header = $('#header'),
+        $all = $body.add($header);
 
-	// Breakpoints.
-		breakpoints({
-			xxlarge: [ '1681px',  '1920px' ],
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '1001px',  '1280px' ],
-			medium:  [ '737px',   '1000px' ],
-			small:   [ '481px',   '736px'  ],
-			xsmall:  [ null,      '480px'  ]
-		});
+    // Breakpoints.
+    breakpoints({
+        xxlarge: [ '1681px',  '1920px' ],
+        xlarge:  [ '1281px',  '1680px' ],
+        large:   [ '1001px',  '1280px' ],
+        medium:  [ '737px',   '1000px' ],
+        small:   [ '481px',   '736px'  ],
+        xsmall:  [ null,      '480px'  ]
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Play initial animations on page load.
+    $window.on('load', function() {
+        setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Touch mode.
-		if (browser.mobile)
-			$body.addClass('is-touch');
-		else {
+    // Touch mode.
+    if (browser.mobile)
+        $body.addClass('is-touch');
+    else {
+        breakpoints.on('<=small', function() {
+            $body.addClass('is-touch');
+        });
 
-			breakpoints.on('<=small', function() {
-				$body.addClass('is-touch');
-			});
+        breakpoints.on('>small', function() {
+            $body.removeClass('is-touch');
+        });
+    }
 
-			breakpoints.on('>small', function() {
-				$body.removeClass('is-touch');
-			});
+    // Fix: IE flexbox fix.
+    if (browser.name == 'ie') {
+        var $main = $('.main.fullscreen'),
+            IEResizeTimeout;
 
-		}
+        $window
+            .on('resize.ie-flexbox-fix', function() {
+                clearTimeout(IEResizeTimeout);
 
-	// Fix: IE flexbox fix.
-		if (browser.name == 'ie') {
+                IEResizeTimeout = setTimeout(function() {
+                    var wh = $window.height();
 
-			var $main = $('.main.fullscreen'),
-				IEResizeTimeout;
+                    $main.each(function() {
+                        var $this = $(this);
 
-			$window
-				.on('resize.ie-flexbox-fix', function() {
+                        $this.css('height', '');
 
-					clearTimeout(IEResizeTimeout);
+                        if ($this.height() <= wh)
+                            $this.css('height', (wh - 50) + 'px');
+                    });
+                });
 
-					IEResizeTimeout = setTimeout(function() {
+            })
+            .triggerHandler('resize.ie-flexbox-fix');
+    }
 
-						var wh = $window.height();
+    // Gallery.
+    $window.on('load', function() {
+        var $gallery = $('.gallery');
 
-						$main.each(function() {
+        $gallery.poptrox({
+            baseZIndex: 10001,
+            useBodyOverflow: false,
+            usePopupEasyClose: false,
+            overlayColor: '#1f2328',
+            overlayOpacity: 0.65,
+            usePopupDefaultStyling: false,
+            usePopupCaption: true,
+            popupLoaderText: '',
+            windowMargin: 50,
+            usePopupNav: true
+        });
 
-							var $this = $(this);
+        // Hack: Adjust margins when 'small' activates.
+        breakpoints.on('>small', function() {
+            $gallery.each(function() {
+                $(this)[0]._poptrox.windowMargin = 50;
+            });
+        });
 
-							$this.css('height', '');
+        breakpoints.on('<=small', function() {
+            $gallery.each(function() {
+                $(this)[0]._poptrox.windowMargin = 5;
+            });
+        });
+    });
 
-							if ($this.height() <= wh)
-								$this.css('height', (wh - 50) + 'px');
+    // Section transitions.
+    if (browser.canUse('transition')) {
 
-						});
+        var on = function() {
+            // Galleries.
+            $('.gallery')
+                .scrollex({
+                    top:        '30vh',
+                    bottom:     '30vh',
+                    delay:      50,
+                    initialize: function() { $(this).addClass('inactive'); },
+                    terminate:  function() { $(this).removeClass('inactive'); },
+                    enter:      function() { $(this).removeClass('inactive'); },
+                    leave:      function() { $(this).addClass('inactive'); }
+                });
 
-					});
+            // Generic sections.
+            $('.main.style1')
+                .scrollex({
+                    mode:       'middle',
+                    delay:      100,
+                    initialize: function() { $(this).addClass('inactive'); },
+                    terminate:  function() { $(this).removeClass('inactive'); },
+                    enter:      function() { $(this).removeClass('inactive'); },
+                    leave:      function() { $(this).addClass('inactive'); }
+                });
 
-				})
-				.triggerHandler('resize.ie-flexbox-fix');
+            $('.main.style2')
+                .scrollex({
+                    mode:       'middle',
+                    delay:      100,
+                    initialize: function() { $(this).addClass('inactive'); },
+                    terminate:  function() { $(this).removeClass('inactive'); },
+                    enter:      function() { $(this).removeClass('inactive'); },
+                    leave:      function() { $(this).addClass('inactive'); }
+                });
 
-		}
+            // Contact.
+            $('#contact')
+                .scrollex({
+                    top:        '50%',
+                    delay:      50,
+                    initialize: function() { $(this).addClass('inactive'); },
+                    terminate:  function() { $(this).removeClass('inactive'); },
+                    enter:      function() { $(this).removeClass('inactive'); },
+                    leave:      function() { $(this).addClass('inactive'); }
+                });
 
-	// Gallery.
-		$window.on('load', function() {
+        };
 
-			var $gallery = $('.gallery');
+        var off = function() {
+            // Galleries.
+            $('.gallery')
+                .unscrollex();
 
-			$gallery.poptrox({
-				baseZIndex: 10001,
-				useBodyOverflow: false,
-				usePopupEasyClose: false,
-				overlayColor: '#1f2328',
-				overlayOpacity: 0.65,
-				usePopupDefaultStyling: false,
-				usePopupCaption: true,
-				popupLoaderText: '',
-				windowMargin: 50,
-				usePopupNav: true
-			});
+            // Generic sections.
+            $('.main.style1')
+                .unscrollex();
 
-			// Hack: Adjust margins when 'small' activates.
-				breakpoints.on('>small', function() {
-					$gallery.each(function() {
-						$(this)[0]._poptrox.windowMargin = 50;
-					});
-				});
+            $('.main.style2')
+                .unscrollex();
 
-				breakpoints.on('<=small', function() {
-					$gallery.each(function() {
-						$(this)[0]._poptrox.windowMargin = 5;
-					});
-				});
+            // Contact.
+            $('#contact')
+                .unscrollex();
+        };
 
-		});
+        breakpoints.on('<=small', off);
+        breakpoints.on('>small', on);
+    }
 
-	// Section transitions.
-		if (browser.canUse('transition')) {
+    // Events.
+    var resizeTimeout, resizeScrollTimeout;
 
-			var on = function() {
+    $window
+        .on('resize', function() {
+            // Disable animations/transitions.
+            $body.addClass('is-resizing');
 
-				// Galleries.
-					$('.gallery')
-						.scrollex({
-							top:		'30vh',
-							bottom:		'30vh',
-							delay:		50,
-							initialize:	function() { $(this).addClass('inactive'); },
-							terminate:	function() { $(this).removeClass('inactive'); },
-							enter:		function() { $(this).removeClass('inactive'); },
-							leave:		function() { $(this).addClass('inactive'); }
-						});
+            clearTimeout(resizeTimeout);
 
-				// Generic sections.
-					$('.main.style1')
-						.scrollex({
-							mode:		'middle',
-							delay:		100,
-							initialize:	function() { $(this).addClass('inactive'); },
-							terminate:	function() { $(this).removeClass('inactive'); },
-							enter:		function() { $(this).removeClass('inactive'); },
-							leave:		function() { $(this).addClass('inactive'); }
-						});
+            resizeTimeout = setTimeout(function() {
+                // Update scrolly links.
+                $('a[href^="#"]').scrolly({
+                    speed: 1500,
+                    offset: $header.outerHeight() - 1
+                });
 
-					$('.main.style2')
-						.scrollex({
-							mode:		'middle',
-							delay:		100,
-							initialize:	function() { $(this).addClass('inactive'); },
-							terminate:	function() { $(this).removeClass('inactive'); },
-							enter:		function() { $(this).removeClass('inactive'); },
-							leave:		function() { $(this).addClass('inactive'); }
-						});
+                // Re-enable animations/transitions.
+                setTimeout(function() {
+                    $body.removeClass('is-resizing');
+                    $window.trigger('scroll');
+                }, 0);
 
-				// Contact.
-					$('#contact')
-						.scrollex({
-							top:		'50%',
-							delay:		50,
-							initialize:	function() { $(this).addClass('inactive'); },
-							terminate:	function() { $(this).removeClass('inactive'); },
-							enter:		function() { $(this).removeClass('inactive'); },
-							leave:		function() { $(this).addClass('inactive'); }
-						});
+            }, 100);
 
-			};
+        })
+        .on('load', function() {
+            $window.trigger('resize');
+        });
 
-			var off = function() {
+    // Dropdown Menu for Mobile
+    var $hamburger = $('#hamburger-icon');
+    var $navMenu = $('.nav-menu');
 
-				// Galleries.
-					$('.gallery')
-						.unscrollex();
-
-				// Generic sections.
-					$('.main.style1')
-						.unscrollex();
-
-					$('.main.style2')
-						.unscrollex();
-
-				// Contact.
-					$('#contact')
-						.unscrollex();
-
-			};
-
-			breakpoints.on('<=small', off);
-			breakpoints.on('>small', on);
-
-		}
-		
-
-	// Events.
-		var resizeTimeout, resizeScrollTimeout;
-
-		$window
-			.on('resize', function() {
-
-				// Disable animations/transitions.
-					$body.addClass('is-resizing');
-
-				clearTimeout(resizeTimeout);
-
-				resizeTimeout = setTimeout(function() {
-
-					// Update scrolly links.
-						$('a[href^="#"]').scrolly({
-							speed: 1500,
-							offset: $header.outerHeight() - 1
-						});
-
-					// Re-enable animations/transitions.
-						setTimeout(function() {
-							$body.removeClass('is-resizing');
-							$window.trigger('scroll');
-						}, 0);
-
-				}, 100);
-
-			})
-			.on('load', function() {
-				$window.trigger('resize');
-			});
+    // Toggle the menu on hamburger click
+    $hamburger.on('click', function() {
+        $navMenu.toggleClass('show');
+    });
 
 })(jQuery);
